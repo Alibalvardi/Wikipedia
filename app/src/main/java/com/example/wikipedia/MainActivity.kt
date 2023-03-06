5ad5fa2f5f8250a8cff699ab3102e4d996815614
Alibalvardi <ali.balvardi.1382@gmail.com>
2023-03-06 19:22:23 +0330

    navigation drawer

--- app/src/main/java/com/example/wikipedia/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.wikipedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.wikipedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolBarMain)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayoutMain,
+            binding.toolBarMain,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        actionBarDrawerToggle.syncState()
+
+        binding.drawerLayoutMain.addDrawerListener(actionBarDrawerToggle)
+
+        binding.navigationDrawerMain.setNavigationItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.menu_writer -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_photographer -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_videoMaker -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_translator -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_visitWikimedia -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+                R.id.menu_visitWikipedia -> {
+                    binding.drawerLayoutMain.closeDrawer(GravityCompat.START)
+                }
+
+            }
+            true
+        }
     }
 }
