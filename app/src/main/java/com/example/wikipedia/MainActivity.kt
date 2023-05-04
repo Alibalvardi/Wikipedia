f3d4c4709ebf42e8467768040bc023f4f4e2cb6b
Alibalvardi <ali.balvardi.1382@gmail.com>
2023-05-04 18:27:16 +0430

    bottom navigation

--- app/src/main/java/com/example/wikipedia/MainActivity.kt
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.example.wikipedia.databinding.ActivityMainBinding
+import com.example.wikipedia.fragments.FragmentExplore
+import com.example.wikipedia.fragments.FragmentProfile
+import com.example.wikipedia.fragments.FragmentTrend
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
             R.string.close_drawer
         )
         actionBarDrawerToggle.syncState()
+
+        firstRun()
 
         binding.drawerLayoutMain.addDrawerListener(actionBarDrawerToggle)
 
@@ -51,5 +57,39 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_explore -> {
+                    replaceFragment(FragmentExplore())
+                }
+
+                R.id.menu_trend -> {
+                    replaceFragment(FragmentTrend())
+                }
+
+                R.id.menu_profile -> {
+                    replaceFragment(FragmentProfile())
+                }
+            }
+
+            true
+        }
+        binding.bottomNavigation.setOnItemReselectedListener {}
+
+
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_main_container, fragment)
+        transaction.commit()
+    }
+
+    fun firstRun() {
+
+        replaceFragment(FragmentExplore())
+        binding.bottomNavigation.selectedItemId = R.id.menu_explore
+
     }
 }
